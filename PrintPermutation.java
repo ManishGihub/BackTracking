def3e316e5ffb0360e06ed4adf511a32a02db296
ManishGihub <102681545+ManishGihub@users.noreply.github.com>
2026-05-20 22:31:03 +0530

    Done

---
+++ PrintPermutation.java
@@ -0,0 +1,25 @@
+Q) Print all permutaions of the string
+
+public class PrintPermutation {
+    public static void PrintPermu(String str, String permu, int idx){
+        if(str.length() == 0){
+            System.out.println(permu);
+            return;
+        }
+        for(int i=0;i<str.length();i++){
+            char currChar = str.charAt(i);
+            String newStr = str.substring(0,i) + str.substring(i+1);
+            PrintPermu(newStr, permu + currChar, idx+1);
+        }
+    }
+    public static void main(String []args){
+        String str = "abc";
+        PrintPermu(str,"",0);
+    }
+}
+// abc
+// acb
+// bac
+// bca
+// cab
+// cba
