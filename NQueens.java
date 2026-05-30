6fa831552450b3b9e535f32a3c447d43ed84b247
ManishGihub <102681545+ManishGihub@users.noreply.github.com>
2026-05-30 18:50:17 +0530

    Update NQueens.java

--- NQueens.java
+++ NQueens.java
@@ -1,32 +1,78 @@
-//Write a java program to print all the solutions to place n queens on nXn chessboard such that all the queens are safe
+// Q)Write a java program to print all the solutions to place n queens on nXn chessboard such that all the queens are safe
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class NQueens{
-    public boolean isSafe(int row,int col,char[][] board){
-        //horizontal
-        for(int j=0;j<board.length;j++){
-            if(board[row][j] == 'Q'){
-                return false;
-            }
-        }
-        //vertical
-        for(int i=0;i<board.length;i++){
-            if(board[j][row] == 'Q'){
-                return false;
-            }
-        }
 
+    public static boolean isSafe(int row,int col,char[][] board){
+        //horizontal
+        for(int i=0;i<board.length;i++){
+            if(board[row][i] == 'Q'){
+                return false;
+            }
+        }
+        //Vertical
+        for(int i=0;i<board[0].length;i++){
+            if(board[i][col] == 'Q'){
+                return false;
+            }
+        }
+        //upper left
+        int r = row;
+        for(int c=col;c>=0 && r>=0;c--,r--){
+            if(board[r][c] == 'Q'){
+                return false;
+            }
+        }
+        //upper right
+        r = row;
+        for(int c=col;c<board.length && r>=0;c++,r--){
+            if(board[r][c] == 'Q'){
+                return false;
+            }
+        }
+        //Lower Left
+        r = row;
+        for(int c = col;c>=0 && r<board.length;c--,r++){
+            if(board[r][c] == 'Q'){
+                return false;
+            }
+        }
+        //Lower right
+        r = row;
+        for(int c=col;c<board.length && r<board.length;c++,r++){
+            if(board[r][c] == 'Q'){
+                return false;
+            }
+        }
+        return true;
     }
 
-    public void helper(char[][] board, List<List<String>> allBoards,int col){
+    public static void saveBoard(char[][] board,List<List<String>> allBoards){
+        String row = "";
+        List<String> newBoard = new ArrayList<>();
+
+        for(int i=0;i<board.length;i++){
+            row = "";
+            for(int j=0;j<board[0].length;j++){
+                if(board[i][j] == 'Q'){
+                    row+='Q';
+                }else{
+                    row+='.';
+                }
+            }
+            newBoard.add(row);
+        }
+        allBoards.add(newBoard);
+    }
+
+    public static void helper(char[][] board,List<List<String>> allBoards,int col){
         if(col == board.length){
-            saveBoard(board, allBoards);
+            saveBoard(board,allBoards);
             return;
         }
-
-        for(int row = 0;row<board.length; row++){
+        for(int row=0;row<board.length;row++){
             if(isSafe(row,col,board)){
                 board[row][col] = 'Q';
                 helper(board, allBoards, col+1);
@@ -35,13 +81,16 @@ public class NQueens{
         }
     }
 
-    public List<List<String>> solveNQueens(int n){
+    public static List<List<String>> solveNqueens(int n){
         List<List<String>> allBoards = new ArrayList<>();
-        char[][] board = new board[n][n]; 
-
-        helper(board, allBoards, 0);
+        char[][] board = new char[n][n];
+        helper(board,allBoards,0);
+        
+        return allBoards;
     }
     public static void main(String[] args) {
-        System.out.println("Hello");
+        System.out.println(solveNqueens(4));
     }
 }
+
+// [[..Q., Q..., ...Q, .Q..], [.Q.., ...Q, Q..., ..Q.]]
